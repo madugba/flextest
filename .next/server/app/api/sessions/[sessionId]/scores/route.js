36691e093656d2350f72c802e4927d9108fde722
Madugba Emmanuel Chinonso <chinonso.madugba@code.berlin>
2026-08-14 14:09:29 +0200

    fix vercel deployment

--- .next/server/app/api/sessions/[sessionId]/scores/route.js
+++
@@ -1,6 +0,0 @@
-var R=require("../../../../../chunks/[turbopack]_runtime.js")("server/app/api/sessions/[sessionId]/scores/route.js")
-R.c("server/chunks/node_modules_next_7232db5e._.js")
-R.c("server/chunks/[root-of-the-server]__ffc3058e._.js")
-R.m("[project]/.next-internal/server/app/api/sessions/[sessionId]/scores/route/actions.js [app-rsc] (server actions loader, ecmascript)")
-R.m("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/src/app/api/sessions/[sessionId]/scores/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)")
-module.exports=R.m("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/src/app/api/sessions/[sessionId]/scores/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)").exports
--- .next/static/chunks/src_app_dashboard_candidates_new_page_tsx_2a13300e._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_e5287af9._.js",
-  "static/chunks/node_modules_zod_v4_606382c0._.js",
-  "static/chunks/node_modules_9488e554._.js"
-],
-    source: "dynamic"
-});
